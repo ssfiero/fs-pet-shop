6c0a2c39d5218fd67518ddaf93fadef3c7519387
Steven Fiero <ssfiero@yahoo.com>
2017-11-29 19:47:44 -0600

    Part 2 Node HTTP Server - All tests passing

--- httpServer.js
+++ httpServer.js
@@ -57,6 +57,40 @@ const server = http.createServer(function(req, res) {
       res.end(petJSON);
     });
   }
+  else if (req.method === 'GET' && req.url === '/pets/2') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsJSON);
+      let petJSON = JSON.stringify(pets[2]);
+
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found');
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/-1') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsJSON);
+      let petJSON = JSON.stringify(pets[-1]);
+
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found');
+    });
+  }
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
