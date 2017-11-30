255ace73e4b10d223af2e93abe4465c8315d0f62
Steven Fiero <ssfiero@yahoo.com>
2017-11-29 19:42:42 -0600

    Part 2 Node HTTP Server - Initial Commit - three tests passing

--- httpServer.js
+++ httpServer.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let fs = require('fs');
 let path = require('path');
 let petsPath = path.join(__dirname, './pets.json');
@@ -21,6 +23,40 @@ const server = http.createServer(function(req, res) {
       res.end(petsJSON);
     });
   }
+  else if (req.method === 'GET' && req.url === '/pets/0') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsJSON);
+      let petJSON = JSON.stringify(pets[0]);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/1') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsJSON);
+      let petJSON = JSON.stringify(pets[1]);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  }
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
@@ -32,8 +68,6 @@ const server = http.createServer(function(req, res) {
 server.listen(port, function() {
   console.log('Listening on port', port);
 });
-
-
 
 
 
