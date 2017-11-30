8aca1c94576fb7f34b32ac9b8258978fec3e9292
Steven Fiero <ssfiero@yahoo.com>
2017-11-30 11:54:12 -0600

    Part 4 RESTful Express Server - Initial Commit - 4 tests passing

---
+++ restfulExpressServer.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let petsPath = path.join(__dirname, './pets.json');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+// middleware
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+
+app.disable('x-powered-by');
+app.use(morgan('short'));
+app.use(bodyParser.json());
+
+
+app.get('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    let pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+
+
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    let id = Number.parseInt(req.params.id);
+    let pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.send(pets[id]);
+  });
+});
+
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+
+
+
+
+
+module.exports = app;
